520696fefd09696624a5f76b95487e2a93f5a796
jenanF <108689302+jenanF@users.noreply.github.com>
2024-12-07 20:58:18 +0300

    done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+
+app.listen(8000, () => {
+    console.log("my application is running! i have no idea how");
+});
+
+
+
+
+
+
+
+
+
+
+
+
