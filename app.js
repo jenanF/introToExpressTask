1dfec9a1dae35177611753fea79527033c18ae08
jenanF <108689302+jenanF@users.noreply.github.com>
2024-12-07 22:56:32 +0300

    done

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello jenan!')
+})
 
 app.listen(8000, () => {
     console.log("my application is running! i have no idea how");
