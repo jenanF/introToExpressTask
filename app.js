ea7a2c10c79b4b2656e85e1a32fcbec5a613b890
jenanF <108689302+jenanF@users.noreply.github.com>
2024-12-07 23:07:20 +0300

    done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello jenan!')
+    res.send({ greet: 'Hello', name: 'jenan!' })
 })
 
 app.listen(8000, () => {
